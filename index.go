3cfae7bda760176c839ec5e3f98b766de0938a8b
mqzaman <mqzaman@gmail.com>
2025-02-13 18:39:12 +0900

    Interface Again

--- index.go
+++ index.go
@@ -7,6 +7,8 @@ import (
 type Walker interface {
 	Walk()
 }
+type I interface {
+}
 type Dog struct {
 	name string
 	age  int
@@ -27,10 +29,22 @@ func (c Cat) Walk() {
 func MakeWalk(w Walker) {
 	w.Walk()
 }
-
+func PrintType(i I) {
+	switch i.(type) {
+	case int:
+		fmt.Println("Integer")
+	case string:
+		fmt.Println("String")
+	default:
+		fmt.Println("Others Type")
+	}
+}
 func main() {
 	dog := Dog{name: "Rex", age: 2}
 	MakeWalk(dog)
 	cat := Cat{name: "Tix", age: 3}
 	MakeWalk(cat)
+	PrintType(5)
+	PrintType("Hello")
+	PrintType(true)
 }
